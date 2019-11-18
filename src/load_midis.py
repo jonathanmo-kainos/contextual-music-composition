2b678452c09588fc00ba3bb67177e5155ec76ea3
jonathanmo-kainos <j.moore@kainos.com>
2019-11-18 23:02:01 +0000

    updating load_midis to reflect midi preprocessing updates

--- src/load_midis.py
+++ src/load_midis.py
@@ -1,20 +1,18 @@
 import midi_preprocessing
 import os
 
-patterns = {}
-midi_dir = r"C:\Users\Jonny\Downloads\Uni project\midis\bigbutt"
+midi_filepath = r"C:\Users\Jonny\Downloads\Uni project\midis\vgmusic"
 all_samples = []
-all_lens = []
-print
-"Loading Songs..."
-for root, subdirs, files in os.walk(midi_dir):
+
+print('Loading midis')
+for root, subdirs, files in os.walk(midi_filepath):
     for file in files:
         path = root + "\\" + file
         if not (path.endswith('.mid') or path.endswith('.midi')):
             continue
         try:
             samples = midi_preprocessing.midi_to_samples(path)
+            all_samples.append(samples)
         except:
-            print
-            "ERROR ", path
+            print('ERROR ', path)
             continue
