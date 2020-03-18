c7d83652d14ffda371f4ad4daae5a5fa88311090
jonathanmo-kainos <j.moore@kainos.com>
2020-03-18 22:25:37 +0000

    Tidied up model training file

--- src/model.py
+++ src/model.py
@@ -1,22 +1,11 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from keras.backend.tensorflow_backend import set_session
 import tensorflow as tf
-from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Input, TimeDistributed, BatchNormalization, Reshape, Dense, Flatten, Activation, \
     Dropout
-from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras.optimizers import RMSprop
-from tensorflow.keras.utils import plot_model
-from tensorflow.keras import backend
 import datetime
-
-config = tf.compat.v1.ConfigProto()
-config.gpu_options.allow_growth = True
-config.gpu_options.per_process_gpu_memory_fraction = 0.9
-config.allow_soft_placement = True
-sess = tf.compat.v1.Session(config=config)
-set_session(sess)
+import enums
 
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
@@ -27,7 +16,7 @@ num_bars = 16
 num_notes = 96
 num_time_samples = 96
 
-midi_matrices = np.load('samples bool.npy')
+midi_matrices = np.load(enums.ALL_SAMPLES_FILEPATH)
 midi_matrices = midi_matrices.reshape(-1, num_bars, num_notes, num_time_samples)
 
 model = Sequential()
@@ -72,12 +61,12 @@ model.add(Reshape((num_bars, num_notes, num_time_samples)))
 
 model.compile(optimizer=RMSprop(lr=0.001), loss='binary_crossentropy')
 
-# plot_model(model, to_file='model.png', show_shapes=True)
+# tensorboard logging for model details
 print(model.summary())
-
 log_directory = "..\logs\\" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_directory, histogram_freq=1)
 
-model.fit(midi_matrices, midi_matrices, batch_size=400, epochs=2000, validation_split=0.05,
+# Train model and save trained model
+model.fit(midi_matrices, midi_matrices, batch_size=100, epochs=40, validation_split=0.05,
           callbacks=[tensorboard_callback])
-model.save(r"C:\Users\Jonny\PycharmProjects\contextual-music-composition\saved models\autoencoder 2000.h5")
+model.save(r"C:\Users\Jonny\PycharmProjects\contextual-music-composition\saved models\autoencoder 40.h5")
