3c8ceb7d9ef86a21c43077cc9d5ecd0e56a76781
jonathanmo-kainos <j.moore@kainos.com>
2020-03-18 22:26:47 +0000

    Added enums file for filepaths and updated references to filepaths in src files

---
+++ src/enums.py
@@ -0,0 +1,16 @@
+# MODELS
+AUTOENCODER_2000_FILEPATH = '../saved models/autoencoder 2000.h5'
+AUTOENCODER_20001_FILEPATH = '../saved models/autoencoder 20001.h5'
+ENCODER_2000_FILEPATH = '../saved models/encoder autoencoder 2000.h5'
+DECODER_2000_FILEPATH = '../saved models/decoder autoencoder 2000.h5'
+
+# SAMPLES
+ALL_SAMPLES_FILEPATH = '../samples/samples.npy'
+MAJOR_SAMPLES_FILEPATH = '../samples/major samples.npy'
+MINOR_SAMPLES_FILEPATH = '../samples/minor samples.npy'
+ALL_ENCODED_SAMPLES_FILEPATH = '../samples/encoded samples/all encoded samples.npy'
+MAJOR_ENCODED_SAMPLES_FILEPATH = '../samples/encoded samples/major encoded samples.npy'
+MINOR_ENCODED_SAMPLES_FILEPATH = '../samples/encoded samples/minor encoded samples.npy'
+
+# RANDOM INPUTS
+RANDOM_INPUT_FILEPATH = '../inputs/randomInput.npy'
